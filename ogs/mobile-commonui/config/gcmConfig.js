f36b7d2cf1fabc086faa5e27aac25a54f76b280d
albinabel365 <67107993+albinabel365@users.noreply.github.com>
2020-06-18 20:23:07 +0530

    Update gcmConfig.js

--- ogs/mobile-commonui/config/gcmConfig.js
+++ ogs/mobile-commonui/config/gcmConfig.js
@@ -1,6 +1,6 @@
 var GCMConfig = (function() {
     // Set this variable to OGS launcher's hostname for the environment.
-    var _launcherHostName = "https://github.com/albinabel365/gdmporting";
+    var _launcherHostName = "https://github.com/albinabel365";
     var _ccy_formatter_url = "https://github.com/albinabel365/gdmporting/ogs/mobile-commonui/js/utils/currencyFormat.js";
     var _translations_url = "https://github.com/albinabel365/gdmporting/ogs/mobile-commonui/js/translations/";
     return {
